2b427f8e15afcd07d170ef24c872ab00f9026074
Levente Ibrányi <ibranyilevente@gmail.com>
2021-12-08 16:33:52 +0100

    fajlBeolvas() add

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -3,12 +3,20 @@ package sample;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
 import javafx.stage.FileChooser;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 
 public class Controller {
 
+    @FXML
+    private ListView<String> lstviewHibak;
+    @FXML
+    private ListView<DHaromszog> lstviewDerekszoguHaromszogek;
     @FXML
     private Button btnAdatokBetoltese;
 
@@ -20,5 +28,29 @@ public class Controller {
                 new FileChooser.ExtensionFilter("Szöveges állományok (*.txt)", "*.txt");
         fileChooser.getExtensionFilters().add(extensionFilter);
         File fajl = fileChooser.showOpenDialog(null);
+        fajlBeolvas(fajl.toString());
+    }
+
+    private void fajlBeolvas(String fajl) {
+        try {
+            FileReader fr = new FileReader(fajl);
+            BufferedReader br = new BufferedReader(fr);
+
+            int i = 1;
+            String sor = br.readLine();
+            while (sor != null) {
+                try {
+                    DHaromszog dh = new DHaromszog(sor, i++);
+                } catch (Exception e) {
+                    lstviewHibak.getItems().add(e.getMessage());
+                } finally {
+                    sor = br.readLine();
+                }
+            }
+            br.close();
+            fr.close();
+        } catch (IOException ioe) {
+            System.err.println(ioe.getMessage());
+        }
     }
 }
