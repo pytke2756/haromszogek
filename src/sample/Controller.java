6b3d8a6478fc0c8eab29319bfc1fe45444a65dcb
Levente Ibrányi <ibranyilevente@gmail.com>
2021-12-08 15:44:59 +0100

    FileChooser add

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,24 @@
 package sample;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.stage.FileChooser;
+
+import java.io.File;
+
 public class Controller {
+
+    @FXML
+    private Button btnAdatokBetoltese;
+
+    @FXML
+    private void onBtnAdatokBetolteseClick(ActionEvent actionEvent) {
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Szöveges állomány megnyitása");
+        FileChooser.ExtensionFilter extensionFilter =
+                new FileChooser.ExtensionFilter("Szöveges állományok (*.txt)", "*.txt");
+        fileChooser.getExtensionFilters().add(extensionFilter);
+        File fajl = fileChooser.showOpenDialog(null);
+    }
 }
